9e1522ff626586696eefb152050b20a0648704d9
zhengsipeng <121569502@qq.com>
2022-03-17 01:27:24 -0700

    3.17

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 python tools/run_net.py \
-    --cfg configs/Kinetics/C2D_8x8_R50.yaml DATA.PATH_TO_DATA_DIR path_to_your_dataset \
+    --cfg configs/Kinetics/C2D_8x8_R50.yaml \
+    DATA.PATH_TO_DATA_DIR path_to_your_dataset \
     TEST.CHECKPOINT_FILE_PATH path_to_your_checkpoint \
     TRAIN.ENABLE False 
